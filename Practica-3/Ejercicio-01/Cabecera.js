33ea1b99b4569d65dc5a13975a895576487a13d5
uo283794 <113892080+uo283794@users.noreply.github.com>
2022-11-23 06:15:58 +0100

    Delete Practica-3 directory

--- Practica-3/Ejercicio-01/Cabecera.js
+++
@@ -1,8 +0,0 @@
-var asignatura = new Object();
-asignatura.nombre = "Software y Estandares en la Web";
-asignatura.titulacion = "Grado en Ingenieria Informatica del Software";
-asignatura.centro = "Escuela de Ingenieria Informatica";
-asignatura.universidad = "Universidad de Oviedo";
-asignatura.curso = "2022-2023";
-asignatura.estudiante = "Daniel Alberto Alonso Fernández";
-asignatura.email = "uo283794@uniovi.es";
