fbefbcb44ca174a1a7484cd11852ca38270e148f
uo283794 <113892080+uo283794@users.noreply.github.com>
2022-11-23 05:52:16 +0100

    Add files via upload

---
+++ Practica-3/Ejercicio-02/MasInfoNavegador.js
@@ -0,0 +1,17 @@
+document.write(infoNavegador.version);
+document.write("</p>");
+
+document.write("<p>");
+document.write(infoNavegador.plataforma);
+document.write("</p>");
+
+document.write("<p>");
+document.write(infoNavegador.vendedor);
+document.write("</p>");
+
+document.write("<p>");
+document.write(infoNavegador.agente);
+document.write("</p>");
+
+document.write("<p>");
+document.write(infoNavegador.javaActivo);
